4a3b87993e730b5e357d9808cb5860f5c11fdd1a
HarryLee87 <haewon.harry.lee@gmail.com>
2023-12-01 11:08:45 -0700

    Created Money Tracking Expense Project

---
+++ App.tsx
@@ -0,0 +1,41 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StyleSheet,
+  Text,
+} from 'react-native';
+
+import {
+  Colors,
+  DebugInstructions,
+  Header,
+  LearnMoreLinks,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+
+
+
+
+function App(): JSX.Element {
+
+  return (
+    <SafeAreaView>
+      <ScrollView>
+        <Text>Money Dancers</Text>
+      </ScrollView>
+    </SafeAreaView>
+  );
+}
+
+const styles = StyleSheet.create({
+});
+
+export default App;
