cff945a9adb41397132148e51527e436dfdac5f5
Haewon (Harry) <haewon.harry.lee@gmail.com>
2023-12-01 22:15:10 -0700

    Created Basic Screens, MainLayOut, and Navigation Bar

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import {SafeAreaView, ScrollView, StyleSheet, Text} from 'react-native';
+import MainLayout from './layouts/MainLayout';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Navigation from './components/Navigation';
+
+function App() {
+
+  const Stack = createNativeStackNavigator();
+
+  return (
+    <MainLayout>
+      <SafeAreaView style={{ flex: 1 }}>
+        <NavigationContainer>
+          <Navigation />
+        </NavigationContainer>
+      </SafeAreaView>
+    </MainLayout>
+  );
+}
+
+const styles = StyleSheet.create({});
+
+export default App;
