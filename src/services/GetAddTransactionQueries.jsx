f75379bbbfa5eb7c8718b222ca4be8b3abd57c06
Laurenchansj <laurenchansj@gmail.com>
2023-12-04 12:34:50 -0700

    organized insert queries into a new file

---
+++ src/services/GetAddTransactionQueries.jsx
@@ -0,0 +1,31 @@
+import db from '../database/OpenDatabase';
+
+export function insertExpenseTransaction({expenseQuery}) {
+  db.transaction(tx => {
+    tx.executeSql(
+      'INSERT INTO expenses (date, amount, description, account_categories_id, expense_categories_id) VALUES (?, ?, ?,?, ?)',
+      expenseQuery,
+      (_, result) => {
+        console.log('Data inserted into expenses successfully from AddScreen');
+      },
+      (_, error) => {
+        console.log(error);
+      },
+    );
+  });
+}
+
+export function insertIncomeTransaction({incomeQuery}) {
+  db.transaction(tx => {
+    tx.executeSql(
+      'INSERT INTO incomes (date, amount, description, account_categories_id, income_categories_id) VALUES (?, ?, ?, ?, ?)',
+      incomeQuery,
+      (_, result) => {
+        console.log('Data inserted into incomes successfully from AddScreen');
+      },
+      (_, error) => {
+        console.log(error);
+      },
+    );
+  });
+}
